4f6ab2807ec915d54acc527a31ca45b58afbe3a3
Gulshan Islam <Gulshan@MAS01.MA-ISB.COM>
2019-09-16 11:33:24 +0500

    dfsaf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-// Run the app by serving the static files dsafda
+// Run the app by serving the static files dsafda asas
 // in the dist directory
 app.use(express.static(__dirname + "/dist/testforlive"));
 
